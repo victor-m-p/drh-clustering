ac4c3a84c27e7d8e12c49a65d33de1c9b6627134
Victor Poulsen <vmp@pop-os.localdomain>
2024-04-16 12:15:14 +0200

    write table of potentially problematic dates for islamic entries

--- problems/islam_dates.py
+++ problems/islam_dates.py
@@ -1,11 +1,7 @@
 import numpy as np
 import pandas as pd
 
-# islam
-
-# christianity
-
-# find christian entries
+# find islamic entries
 entry_tags = pd.read_csv("../data/raw/entry_tags.csv")
 entry_string_match = "Islam"
 
@@ -19,12 +15,14 @@ islam_entries = (
 entry_metadata = pd.read_csv("../data/preprocessed/entry_metadata.csv")
 entry_metadata = entry_metadata[["entry_id", "entry_name", "year_from", "year_to"]]
 islam_metadata = entry_metadata[entry_metadata["entry_id"].isin(islam_entries)]
-islam_metadata.sort_values("year_from").head(20)
 
+# we additionally need expert, editor
+raw_data = pd.read_csv("../data/raw/data_dump.csv")
+raw_data = raw_data[
+    ["entry_id", "expert_id", "expert_name", "editor_id", "editor_name"]
+].drop_duplicates()
+islam_metadata = islam_metadata.merge(raw_data, on="entry_id", how="inner")
+islam_problematic = islam_metadata.sort_values("year_from", ascending=True).head(20)
 
-# find non-christian entries
-non_christian_entries = (
-    entry_tags[~entry_tags["entry_id"].isin(christian_entries)]["entry_id"]
-    .unique()
-    .tolist()
-)
+# write potentially problematic csv files
+islam_problematic.to_csv("islam_metadata.csv", index=False)
